1bc2012d5b2abd42510be0a974935054e28f2df4
willi-dev <willi.ilmukomputer@gmail.com>
2019-10-10 00:49:01 +0700

    [dev/create-component] create base pokemon & pokemonlist component

---
+++ src/components/pokemon.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Pokemon = () => (
+  <div>Pokemon here!</div>
+)
+
+export default Pokemon
---
+++ src/components/pokemonlist.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const PokemonList = () => (
+  <div>Pokemon list here</div>
+)
+
+export default PokemonList
