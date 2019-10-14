730d3203e06d7c103eda74a3bccfca34f6aea804
willi-dev <willi.ilmukomputer@gmail.com>
2019-10-14 17:53:05 +0700

    [dev/enhance-detail-monster] fixing box detail loading

--- src/components/loading/box-detail.js
+++ src/components/loading/box-detail.js
@@ -10,7 +10,7 @@ const styleText = {
 const BoxDetail = () => (
   <div className="w-full md:w-1/3 wrounded overflow-hidden">
     <div className="shadow-lg mx-5 mb-5 px-4 py-4">
-      <img src="https://place-hold.it/500x500" alt="monster" />
+      <div className="w-full h-64" style={styleText}>Loading...</div>
       <p className="font-bold text-xl mb-2" style={styleText}>Loading Monster....</p>
       <span style={styleText} className="text-xs inline-block mb-1 bg-gray-200 rounded-full px-3 py-1 font-semibold text-gray-700 mr-2">.........</span>
       <p className="text-gray-700 text-base mb-1" style={styleText}>
